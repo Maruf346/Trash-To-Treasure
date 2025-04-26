7ba4b588b83256e4d7662d7464f092ac4d6d8ac2
Maruf Hossain <maruf.bshs@gmail.com>
2025-04-27 00:46:31 +0600

    registered other models to admin

--- waste_marketplace/marketplace/admin.py
+++ waste_marketplace/marketplace/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import TrashItem
-from .models import TrashItem, UpcycledProduct
+from .models import TrashItem, UpcycledProduct, Order, OrderItem, CartItem 
 
 
 admin.site.register(TrashItem)
 admin.site.register(UpcycledProduct)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(CartItem)
 # Register your models here.
